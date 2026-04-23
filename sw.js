df7a20a6218d6bfcdca24f0843aea74ee9f12942
combark-max <combark@gmail.com>
2026-04-23 17:51:16 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,5 @@
-const CACHE = 'diary-pwa-v3';
-const ASSETS = [
-  '/Bright-Life/',
-  '/Bright-Life/index.html',
-  '/Bright-Life/manifest.json'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
-});
+const CACHE = 'diary-pwa-v4';
+const ASSETS = ['/Bright-Life/', '/Bright-Life/index.html', '/Bright-Life/manifest.json'];
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
+self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))); });
